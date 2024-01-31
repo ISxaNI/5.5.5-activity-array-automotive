dd71c85f4d586462b3226941dacee12c40019107
ISxaNI <jasonliu99999@yahoo.com>
2024-01-30 17:51:51 -0800

    save

--- mercurySedan.js
+++ mercurySedan.js
@@ -5,18 +5,56 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log("You do not have enough space to fit everyone in this" + this.model + " " + this.make);
 
-//After you write the derived Car class, you should test it out.
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("It ain't got no gas in it");
+            return this.started = false;
+        }
+    }
+    checkService() {
+        if (this.mileage > 30000) {    
+            console.log("Your Vehicle is Due for Maintanence!")        
+            this.scheduleService = true
+            return this.scheduleService;                       
+        } else {
+            console.log("you have " + (mileage  - this.mileage) + " until your next required Maintenace")
+            this.scheduleService = false
+            return this.scheduleService;
+        }
+    }
+}
 
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
+let fastBoi = new Car('mercury', 'sedan', '1965', 'burgundy', '27965')
 
-
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
+fastBoi.loadPassenger()
+fastBoi.start()
+fastBoi.checkService()
 
 
 
